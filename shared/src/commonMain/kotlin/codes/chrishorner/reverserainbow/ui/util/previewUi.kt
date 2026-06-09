32eebdbd9c31f08d19edbe0eeb2611d4f97f6f38
Chris Horner <chris@horner.chat>
2026-06-09 11:28:50 +1000

    Add convenience composable wrapper for previews

---
+++ shared/src/commonMain/kotlin/codes/chrishorner/reverserainbow/ui/util/previewUi.kt
@@ -0,0 +1,31 @@
+package codes.chrishorner.reverserainbow.ui.util
+
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.SharedTransitionLayout
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Modifier
+import codes.chrishorner.reverserainbow.ui.LocalAnimatedContentScope
+import codes.chrishorner.reverserainbow.ui.LocalSharedTransitionScope
+
+/**
+ * Render the app background, and provide some defaults for composition locals.
+ */
+@Composable
+fun PreviewUi(content: @Composable () -> Unit) {
+  SharedTransitionLayout {
+    AnimatedContent(targetState = Unit) { _ ->
+      CompositionLocalProvider(
+        LocalSharedTransitionScope provides this@SharedTransitionLayout,
+        LocalAnimatedContentScope provides this@AnimatedContent,
+      ) {
+        Box(modifier = Modifier.background(MaterialTheme.colorScheme.background)) {
+          content()
+        }
+      }
+    }
+  }
+}

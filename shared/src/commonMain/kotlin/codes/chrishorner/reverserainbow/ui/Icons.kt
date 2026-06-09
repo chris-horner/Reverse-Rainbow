b2645e303c192cd8a58c0a10d532807b8b95f67c
Chris Horner <chris@horner.chat>
2026-06-09 11:30:55 +1000

    Add delete and more icons

--- shared/src/commonMain/kotlin/codes/chrishorner/reverserainbow/ui/Icons.kt
+++ shared/src/commonMain/kotlin/codes/chrishorner/reverserainbow/ui/Icons.kt
@@ -911,4 +911,134 @@ object Icons {
     }
 
   private var _Check: ImageVector? = null
+
+  val Delete: ImageVector
+    get() {
+      if (_Delete != null) {
+        return _Delete!!
+      }
+      _Delete = ImageVector.Builder(
+        name = "Delete",
+        defaultWidth = 24.dp,
+        defaultHeight = 24.dp,
+        viewportWidth = 960f,
+        viewportHeight = 960f
+      ).apply {
+        path(fill = SolidColor(Color(0xFF1F1F1F))) {
+          moveTo(280f, 840f)
+          quadToRelative(-33f, 0f, -56.5f, -23.5f)
+          reflectiveQuadTo(200f, 760f)
+          verticalLineToRelative(-520f)
+          quadToRelative(-17f, 0f, -28.5f, -11.5f)
+          reflectiveQuadTo(160f, 200f)
+          quadToRelative(0f, -17f, 11.5f, -28.5f)
+          reflectiveQuadTo(200f, 160f)
+          horizontalLineToRelative(160f)
+          quadToRelative(0f, -17f, 11.5f, -28.5f)
+          reflectiveQuadTo(400f, 120f)
+          horizontalLineToRelative(160f)
+          quadToRelative(17f, 0f, 28.5f, 11.5f)
+          reflectiveQuadTo(600f, 160f)
+          horizontalLineToRelative(160f)
+          quadToRelative(17f, 0f, 28.5f, 11.5f)
+          reflectiveQuadTo(800f, 200f)
+          quadToRelative(0f, 17f, -11.5f, 28.5f)
+          reflectiveQuadTo(760f, 240f)
+          verticalLineToRelative(520f)
+          quadToRelative(0f, 33f, -23.5f, 56.5f)
+          reflectiveQuadTo(680f, 840f)
+          lineTo(280f, 840f)
+          close()
+          moveTo(680f, 240f)
+          lineTo(280f, 240f)
+          verticalLineToRelative(520f)
+          horizontalLineToRelative(400f)
+          verticalLineToRelative(-520f)
+          close()
+          moveTo(428.5f, 668.5f)
+          quadTo(440f, 657f, 440f, 640f)
+          verticalLineToRelative(-280f)
+          quadToRelative(0f, -17f, -11.5f, -28.5f)
+          reflectiveQuadTo(400f, 320f)
+          quadToRelative(-17f, 0f, -28.5f, 11.5f)
+          reflectiveQuadTo(360f, 360f)
+          verticalLineToRelative(280f)
+          quadToRelative(0f, 17f, 11.5f, 28.5f)
+          reflectiveQuadTo(400f, 680f)
+          quadToRelative(17f, 0f, 28.5f, -11.5f)
+          close()
+          moveTo(588.5f, 668.5f)
+          quadTo(600f, 657f, 600f, 640f)
+          verticalLineToRelative(-280f)
+          quadToRelative(0f, -17f, -11.5f, -28.5f)
+          reflectiveQuadTo(560f, 320f)
+          quadToRelative(-17f, 0f, -28.5f, 11.5f)
+          reflectiveQuadTo(520f, 360f)
+          verticalLineToRelative(280f)
+          quadToRelative(0f, 17f, 11.5f, 28.5f)
+          reflectiveQuadTo(560f, 680f)
+          quadToRelative(17f, 0f, 28.5f, -11.5f)
+          close()
+          moveTo(280f, 240f)
+          verticalLineToRelative(520f)
+          verticalLineToRelative(-520f)
+          close()
+        }
+      }.build()
+
+      return _Delete!!
+    }
+
+  private var _Delete: ImageVector? = null
+
+  val MoreHoriz: ImageVector
+    get() {
+      if (_MoreHoriz != null) {
+        return _MoreHoriz!!
+      }
+      _MoreHoriz = ImageVector.Builder(
+        name = "MoreHoriz",
+        defaultWidth = 24.dp,
+        defaultHeight = 24.dp,
+        viewportWidth = 960f,
+        viewportHeight = 960f
+      ).apply {
+        path(fill = SolidColor(Color.Black)) {
+          moveTo(240f, 560f)
+          quadToRelative(-33f, 0f, -56.5f, -23.5f)
+          reflectiveQuadTo(160f, 480f)
+          quadToRelative(0f, -33f, 23.5f, -56.5f)
+          reflectiveQuadTo(240f, 400f)
+          quadToRelative(33f, 0f, 56.5f, 23.5f)
+          reflectiveQuadTo(320f, 480f)
+          quadToRelative(0f, 33f, -23.5f, 56.5f)
+          reflectiveQuadTo(240f, 560f)
+          close()
+          moveTo(480f, 560f)
+          quadToRelative(-33f, 0f, -56.5f, -23.5f)
+          reflectiveQuadTo(400f, 480f)
+          quadToRelative(0f, -33f, 23.5f, -56.5f)
+          reflectiveQuadTo(480f, 400f)
+          quadToRelative(33f, 0f, 56.5f, 23.5f)
+          reflectiveQuadTo(560f, 480f)
+          quadToRelative(0f, 33f, -23.5f, 56.5f)
+          reflectiveQuadTo(480f, 560f)
+          close()
+          moveTo(720f, 560f)
+          quadToRelative(-33f, 0f, -56.5f, -23.5f)
+          reflectiveQuadTo(640f, 480f)
+          quadToRelative(0f, -33f, 23.5f, -56.5f)
+          reflectiveQuadTo(720f, 400f)
+          quadToRelative(33f, 0f, 56.5f, 23.5f)
+          reflectiveQuadTo(800f, 480f)
+          quadToRelative(0f, 33f, -23.5f, 56.5f)
+          reflectiveQuadTo(720f, 560f)
+          close()
+        }
+      }.build()
+
+      return _MoreHoriz!!
+    }
+
+  private var _MoreHoriz: ImageVector? = null
 }

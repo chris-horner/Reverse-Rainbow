474171a6157d950f745e9e355739a8c79275212f
Chris Horner <chris@horner.chat>
2026-08-28 14:02:32 +1000

    Fix layout and mode not recalculating when they should

--- shared/src/commonMain/kotlin/codes/chrishorner/reverserainbow/ui/UiLayoutManager.kt
+++ shared/src/commonMain/kotlin/codes/chrishorner/reverserainbow/ui/UiLayoutManager.kt
@@ -3,9 +3,6 @@ package codes.chrishorner.reverserainbow.ui
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.derivedStateOf
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.min
@@ -31,22 +28,14 @@ val LocalUiMode = staticCompositionLocalOf { UiMode.Small }
 @Composable
 fun UiLayoutManager(content: @Composable () -> Unit) {
   BoxWithConstraints {
-    val layout by remember {
-      derivedStateOf {
-        when {
-          maxHeight < maxWidth && maxHeight < 700.dp -> LayoutOrientation.Landscape
-          else -> LayoutOrientation.Portrait
-        }
-      }
+    val layout = when {
+      maxHeight < maxWidth && maxHeight < 700.dp -> LayoutOrientation.Landscape
+      else -> LayoutOrientation.Portrait
     }
 
-    val uiMode by remember {
-      derivedStateOf {
-        when {
-          min(maxWidth, maxHeight) > 700.dp -> UiMode.Large
-          else -> UiMode.Small
-        }
-      }
+    val uiMode = when {
+      min(maxWidth, maxHeight) > 700.dp -> UiMode.Large
+      else -> UiMode.Small
     }
 
     CompositionLocalProvider(
